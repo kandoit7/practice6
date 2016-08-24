98cc7edf1fe4b319b5de1609d3c4b79c871f5785
kandoit7 <jmin8884@gmail.com>
2016-08-24 15:08:28 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,9 +5,10 @@ function recordTest () {};
 //var audioRecorder = null;
 var test1 = null;
 
-/*
+
 function gotStream(stream) {
 	
+	console.log(stream);
 	// Create an AudioNode from the stream.
 	var realAudioInput = audioContext.createMediaStreamSource(stream);
 	var audioInput = realAudioInput;
@@ -27,7 +28,7 @@ function gotStream(stream) {
 	inputPoint.connect(audioContext.destination);
 	return audioRecorder;
 }
-*/
+
 function initAudio(index) {
 
 	var audioSource = index.value;
@@ -35,30 +36,7 @@ function initAudio(index) {
 	var constraints = {
 		audio: { deviceId: audioSource ? {exact: audioSource} : undefined}
 	
-	this.gotStream = function (stream) {
-		
-		// Create an AudioNode from the stream.
-		var realAudioInput = audioContext.createMediaStreamSource(stream);
-		var audioInput = realAudioInput;
-		
-		var inputPoint = audioContext.createGain();
-		inputPoint.gain.value = 1.0;
-		audioInput.connect(inputPoint);
-		//audioInput = convertToMono( input );
-		
-		var analyserNode = audioContext.createAnalyser();
-		analyserNode.fftSize = 2048;
-		inputPoint.connect( analyserNode );
-		
-		audioRecorder = new Recorder( inputPoint ); // this fuck what the fuck
-		// speak / headphone feedback initial settings
-		
-		inputPoint.connect(audioContext.destination);
-		return audioRecorder;
-	}
-	
 	navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
-	return audioRecorder;
 }
 
 function gotDevices(deviceInfos) {
