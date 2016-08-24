05ac1c1bdac76e8923142c13bdd4a14afa0c78b0
kandoit7 <jmin8884@gmail.com>
2016-08-24 18:00:50 +0900

    Update recorder.js

--- javascripts/recorderjs/recorder.js
+++ javascripts/recorderjs/recorder.js
@@ -108,10 +108,10 @@ DEALINGS IN THE SOFTWARE.
 
   Recorder.setupDownload = function(blob, filename){
     var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    console.log(this.node);
     var link = document.getElementById("save");
     link.href = url;
     link.download = filename || 'output.wav';
+    return url;
   }
 
   window.Recorder = Recorder;
