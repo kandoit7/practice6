255664459b2b42c10627b53ea8908e17fffd897b
kandoit7 <jmin8884@gmail.com>
2016-08-24 12:04:57 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,85 +3,85 @@ var masterInputSelector = document.createElement('select');
 
 var recordTest = {
 
-recordTest.audioRecorder = null;
-recordTest.recIndex = 0;
-recordTest.canvasID = null;
-recordTest.lrecord = null;
-recordTest.tracklink = null;
-recordTest.downlink = null;
-
-recordTest.gotBuffer = function(buffers) {
-	var ci = "c"+canvasID;
-	var canvas = document.getElementById(ci);
-	drawBuffer(canvas.width, canvas.height, canvas.getContext('2d'), buffers[0]);
-	audioRecorder.exportWAV(doneEncoding);
-}
-
-recordTest.doneEncoding = function(blob) {
-	Recorder.setupDownload(blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav");
-	recIndex++;
-}
-
-recordTest.play = function(e) {
-	var track = new Audio(tracklink.href);
-}
-
-recordTest.down = function(e) {
-	tracklink = document.createElement('a');
-	tracklink.id = lrecord;
-	tracklink.href = downlink.href;
-	e.appendChild(tracklink);
-}
-
-recordTest.toggleRecording = function(e) {
-	canvasID = e.id;
-	var imgChange = e;
-	if(e.classList.containts("recording")) {
-		audioRecorder.stop();
-		e.classList.remove("recording");
-		imgChange.src = 'images/mic.png';
-		lrecord = "l" + e.id;
-		audioRecorder.getBuffers(gotBuffer);
-		downlink = document.getElementById('save');
-	} else {
-		if(!audioRecorder)
-			return;
-		
-		e.classList.add("recording");
-		imgChange.src = 'images/micrec.png';
-		audioRecorder.clear();
-		audioRecorder.record();
+	recordTest.audioRecorder = null;
+	recordTest.recIndex = 0;
+	recordTest.canvasID = null;
+	recordTest.lrecord = null;
+	recordTest.tracklink = null;
+	recordTest.downlink = null;
+	
+	recordTest.gotBuffer = function(buffers) {
+		var ci = "c"+canvasID;
+		var canvas = document.getElementById(ci);
+		drawBuffer(canvas.width, canvas.height, canvas.getContext('2d'), buffers[0]);
+		audioRecorder.exportWAV(doneEncoding);
 	}
-}
-
-recordTest.initAudio = function(index) {
-	var audioSource = index.value;
-	var constraints = {
-		audio: {deviceId: audioSource ? {exact: audioSource} : undefined}
-	};
 	
-	navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
-}
-
-recordTest.gotStream = function(stream) {
+	recordTest.doneEncoding = function(blob) {
+		Recorder.setupDownload(blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav");
+		recIndex++;
+	}
 	
-	var realAudioInput = audioContext.createMediaStreamSource(stream);
-	var audioInput = realAudioInput;
+	recordTest.play = function(e) {
+		var track = new Audio(tracklink.href);
+	}
 	
-	var inputPoint = audioContext.createGain();
-	inputPoint.gain.value = 1.0;
-	audioInput.connect(inputPoint);
-	//audioInput = convertToMono( input );
+	recordTest.down = function(e) {
+		tracklink = document.createElement('a');
+		tracklink.id = lrecord;
+		tracklink.href = downlink.href;
+		e.appendChild(tracklink);
+	}
 	
-	var analyserNode = audioContext.createAnalyser();
-	analyserNode.fftSize = 2048;
-	inputPoint.connect( analyserNode );
+	recordTest.toggleRecording = function(e) {
+		canvasID = e.id;
+		var imgChange = e;
+		if(e.classList.containts("recording")) {
+			audioRecorder.stop();
+			e.classList.remove("recording");
+			imgChange.src = 'images/mic.png';
+			lrecord = "l" + e.id;
+			audioRecorder.getBuffers(gotBuffer);
+			downlink = document.getElementById('save');
+		} else {
+			if(!audioRecorder)
+				return;
+			
+			e.classList.add("recording");
+			imgChange.src = 'images/micrec.png';
+			audioRecorder.clear();
+			audioRecorder.record();
+		}
+	}
 	
-	audioRecorder = new Recorder( inputPoint ); // this fuck what the fuck
+	recordTest.initAudio = function(index) {
+		var audioSource = index.value;
+		var constraints = {
+			audio: {deviceId: audioSource ? {exact: audioSource} : undefined}
+		};
+		
+		navigator.mediaDevices.getUserMedia(constraints).then(gotStream).catch(handleError);
+	}
 	
-	inputPoint.connect(audioContext.destination);
-	
-}
+	recordTest.gotStream = function(stream) {
+		
+		var realAudioInput = audioContext.createMediaStreamSource(stream);
+		var audioInput = realAudioInput;
+		
+		var inputPoint = audioContext.createGain();
+		inputPoint.gain.value = 1.0;
+		audioInput.connect(inputPoint);
+		//audioInput = convertToMono( input );
+		
+		var analyserNode = audioContext.createAnalyser();
+		analyserNode.fftSize = 2048;
+		inputPoint.connect( analyserNode );
+		
+		audioRecorder = new Recorder( inputPoint ); // this fuck what the fuck
+		
+		inputPoint.connect(audioContext.destination);
+		
+	}
 }
 
 function gotDevices(deviceInfos) {
@@ -113,6 +113,6 @@ function handleError(error) {
   console.log('navigator.getUserMedia error: ', error);
 }
 
-var firstRecord = new recordTest();
-
 navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+
+var firstRecord = new recordTest();
