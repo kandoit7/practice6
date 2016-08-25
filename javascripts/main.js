9a7b34473e7329766d82016539154896065093a4
kandoit7 <jmin8884@gmail.com>
2016-08-25 10:47:46 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -31,10 +31,11 @@ function toggleRecording( e ) {
 			//e.parentNode.src.exportWAV(doneEncoding);
 			e.parentNode.src.exportWAV(function(blob) {
 				var good = Recorder.setupDownload( blob );
-				var link = document.getElementById("save");
+				var link = document.createElement("a");
 				link.href = good;
 				link.download =  "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav"  || 'output.wav';
 				recIndex++;
+				e.parentNode.childNode(link);
 			});
 			
 		});
