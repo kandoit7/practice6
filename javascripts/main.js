cd1e1cc3ef36aed9de66e09aa45a4d103284f763
kandoit7 <jmin8884@gmail.com>
2016-08-26 10:18:52 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,7 +23,7 @@ function addNewTrack(e) {
 		+ "	</span>"
 		+ "	<canvas id='crecord" + tagIndex + "'" + "class='record" + tagIndex + "'" + "></canvas>"
 		+ "	<img id='record" + tagIndex + "'" + "src='images/mic.png' onclick='toggleRecording(this);'>"
-		+ "	<select></select>"
+		+ "	<select id='change'></select>"
 		+ "	<a></a>";
 	
 	var lastProc = document.getElementById("track"+tagIndex);
