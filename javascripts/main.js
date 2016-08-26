9103701247924f0524b3f7f63848701d82d17a9d
kandoit7 <jmin8884@gmail.com>
2016-08-26 10:06:23 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,7 +15,7 @@ function addNewTrack(e) {
 	newDiv.id = "track"+tagIndex;
 	var node = e.parentNode.parentNode;
 	node.insertBefore(newDiv, node.childNodes[4]);
-	console.log(node.childNodes[10]);
+	console.log(node.childNodes[11]);
 	
 	var tag = "	<span class='track'>track" + tagIndex 
 	  	+ "		<input type='button' value='play' onclick='play(this);'/> "
