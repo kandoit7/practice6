b8a54335a20af965505dd857676a6afcc3e16f8d
kandoit7 <jmin8884@gmail.com>
2016-08-26 10:09:41 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,7 +14,7 @@ function addNewTrack(e) {
 	var newDiv = document.createElement('div');
 	newDiv.id = "track"+tagIndex;
 	var node = e.parentNode.parentNode;
-	node.insertBefore(newDiv, node.childNodes[10]);
+	node.insertBefore(newDiv, node.childNodes[10+childIndex]);
 	console.log(node.childNodes[10]);
 	
 	var tag = "	<span class='track'>track" + tagIndex 
