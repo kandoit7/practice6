8f85276e7c3330c9a980b9f7ef5d3b0ba9212b4c
kandoit7 <jmin8884@gmail.com>
2016-08-30 16:32:59 +0900

    Update midimain.js

--- javascripts/midimain.js
+++ javascripts/midimain.js
@@ -1,4 +1,14 @@
 
 if (navigator.requestMIDIAccess) {
     console.log('Browser supports MIDI!');
+    navigator.requestMIDIAccess()
+        .then(success, failure);
+}
+
+function success (midi) {
+    console.log('Got midi!', midi);
+}
+ 
+function failure () {
+    console.error('No access to your midi devices.')
 }
