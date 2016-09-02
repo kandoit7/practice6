0ca4a84107dca218470d88505b7eb235b4d0c003
kandoit7 <jmin8884@gmail.com>
2016-09-02 17:17:44 +0900

    Update record.js

--- javascripts/record.js
+++ javascripts/record.js
@@ -13,6 +13,7 @@ var deviceSF = document.getElementById('deviceSF');
 //canvas drag & drop
 window.onload=function(){
 	var canvas = document.querySelectorAll('canvas');
+	console.log(canvas);
 	var ctx = canvas.getContext('2d');
 	var w = canvas.width;
 	var h = canvas.height;
@@ -53,38 +54,6 @@ window.onload=function(){
 		//read the file
 		reader.readAsArrayBuffer(e.dataTransfer.files[0]);
 	});
-	//program our analyser
-	analyser.onaudioprocess = function(e){
-		var out = e.outputBuffer.getChannelData(0);
-		var int = e.inputBuffer.getChannelData(0);
-		var max = 0;
-		
-		for(var i = 0; i < int.length; i++){
-			out[i] = 0;//prevent feedback and we only need the input data
-			max = int[i] > max ? int[i] : max;
-		}
-		//convert from magitude to decibel
-		var db = 20*Math.log(Math.max(max,Math.pow(10,-72/20)))/Math.LN10;
-		//It's time to draw on the canvas
-		//create the gradient
-		var grad = ctx.createLinearGradient(w/10,h*0.2,w/10,h*0.95);
-		grad.addColorStop(0,'red');
-		grad.addColorStop(-6/-72,'yellow');
-		grad.addColorStop(1,'green');
-		//fill the background
-		ctx.fillStyle = '#555';
-		ctx.fillRect(0,0,w,h);
-		ctx.fillStyle = grad;
-		//draw the rectangle
-		ctx.fillRect(w/10,h*0.8*(db/-72),w*8/10,(h*0.95)-h*0.8*(db/-72));
-		//draw the text out
-		ctx.fillStyle="white";
-		ctx.font = "Arial 12pt";
-		ctx.textAlign = "center";
-		ctx.fillText(Math.round(db*100)/100+' dB',w/2,h-h*0.025);
-	};
-	//Now try to drag some audio files to the preview area
-	//it'll show a nice audio meter
 }
 
 //all track Recording function
