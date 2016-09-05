dda1ee1a2d1bb8764d3f2a5c8723cd52a3155c27
kandoit7 <jmin8884@gmail.com>
2016-09-05 15:12:53 +0900

    Update record.js

--- javascripts/record.js
+++ javascripts/record.js
@@ -157,24 +157,11 @@ function play(e) {
 	}
 }
 
-//click play button -> play the recorded audio
+//click stop button -> play the recorded audio
 function stop(e) {
-	if(e.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.href === "") {
-		console.log("no recorded audio");
-	} else {
-		var link = e.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.cloneNode(true);
-		var parentLink = e.parentNode;
-		var a = document.createElement('a');
-		a = link;
-		var existA = e.nextElementSibling.nextElementSibling;
-		var img = document.createElement('img');
-		img.id = 'save';
-		img.src = 'images/save.png';
-		a.appendChild(img);
-		parentLink.replaceChild(a, existA);
-		var track = new Audio(link.href);
-		track.stop();
-	}
+
+	var link = e.nextElementSibling;
+	console.log(link);
 	
 	var canvasTrack =  e.parentNode.nextElementSibling.src;
 	canvasTrack.stop();
